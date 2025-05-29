c6e66d16a82583f176e9a9d14d4213df927bd0a4
Harshil Valiya <harshilvaliya40@gmail.com>
2025-05-29 19:39:28 +0530

    s

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // fallback to index.html on dev server
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', // ensures correct input entry
+    },
+  },
 });
