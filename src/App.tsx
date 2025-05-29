03d60f1ce684d2da52939faf0bceb1fcb4ac5316
Harshil Valiya <harshilvaliya40@gmail.com>
2025-05-29 21:16:49 +0530

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/studio" element={<Studio />} />
+        <Route path="/harshil" element={<Studio />} />
+
       </Routes>
     </Router>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // fallback to index.html on dev server
-  },
-  build: {
-    rollupOptions: {
-      input: 'index.html', // ensures correct input entry
-    },
-  },
 });
