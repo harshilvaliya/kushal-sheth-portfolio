fe27e6dee9378b01077a15131a96735585f033eb
Harshil Valiya <harshilvaliya40@gmail.com>
2025-05-29 19:22:18 +0530

    3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import { motion } from "framer-motion";
 import { MdOutlineArrowForward } from "react-icons/md";
 import KushalShethImg from "../assets/kushal-sheth-img.webp";
+import { Navigate } from "react-router-dom";
 
 const Hero: React.FC = () => {
   return (
@@ -36,10 +37,7 @@ const Hero: React.FC = () => {
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
                 href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  window.location.href = "/studio";
-                }}
+                onClick={() => Navigate("/studio")}
                 className="inline-flex items-center px-6 py-3 text-white bg-red-600 rounded-lg transition-colors hover:bg-red-700"
               >
                 Red & Black Studio
